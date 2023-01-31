e67d45ad955fb5d3eece4805953600a4986b9604
Astronaaa <13018052569@163.com>
2023-01-31 10:51:09 +0100

    export csv

--- custom/custom.js
+++ custom/custom.js
@@ -14,59 +14,108 @@ $(document).ready(function() {
         let data = [];
 
 
-        let IDdata = [];
+        //let IDdata = [];
         let ID = document.getElementById("testID").value;
         if(!!ID){
-            IDdata['experimentID'] = ID;
-            data.push(IDdata);
+            data['experimentID'] = ID;
+            //data.push(IDdata);
         }else{
             alert("Bitte Aufgabe ID hinzufügen!");
             //return false;
         }
-        
+
 
         let SUSQuestions = document.getElementById("SUSForm").getElementsByClassName("mb-3");
-        let SUSData = [];
+        //let SUSData = [];
         for(let i=0; i<SUSQuestions.length; i++){
            let SUSID = SUSQuestions[i].id;
            let checkedItem = SUSQuestions[i].querySelector('.form-check-input:checked');
            if(!!checkedItem){
             let checkedValue = checkedItem.value;
-            SUSData[SUSID] = checkedValue;
+            data[SUSID] = checkedValue;
            }else{
             alert("Bitte alle SUS eingeben!");
             return false;
            }       
         }
-        data.push(SUSData);
+        //data.push(SUSData);
         
 
         let UEQSQuestions = document.getElementById("UEQForm").getElementsByClassName("mb-3");
-        let UEQData = [];
+        //let UEQData = [];
         for(let j=0; j<UEQSQuestions.length; j++){
             let UEQID = UEQSQuestions[j].id;
             let checkedItem = UEQSQuestions[j].querySelector('.form-check-input:checked');
             if(!!checkedItem){
                 let checkedValue = checkedItem.value;
-                UEQData[UEQID] = checkedValue;
+                data[UEQID] = checkedValue;
             }else{
                 alert("Bitte alle UEQ-S eingeben!");
                 return false;
             }    
         }
-        data.push(UEQData);
+        //data.push(UEQData);
 
 
         let NASA_TLXQuestions = document.getElementById("NASA_TLXForm").getElementsByClassName("mb-3");
-        let NASA_TLXData = [];
+        //let NASA_TLXData = [];
         for(let l=0; l<NASA_TLXQuestions.length; l++){
             let NASA_TLXID = NASA_TLXQuestions[l].id;
             let rangeValue = NASA_TLXQuestions[l].querySelector('input').value;
-            NASA_TLXData[NASA_TLXID] = rangeValue;
+            data[NASA_TLXID] = rangeValue;
         }
-        data.push(NASA_TLXData);
-        
-
-        console.log(data);
+        //data.push(NASA_TLXData);
+                
+        const csvdata = csvmaker(data);
+        download(csvdata, ID);
+    
     })
-})
+});
+
+//https://www.geeksforgeeks.org/how-to-create-and-download-csv-file-in-javascript/
+const download = function (data, ID) {
+ 
+    // Creating a Blob for having a csv file format
+    // and passing the data with type
+    const blob = new Blob([data], { type: 'text/csv' });
+ 
+    // Creating an object for downloading url
+    const url = window.URL.createObjectURL(blob)
+ 
+    // Creating an anchor(a) tag of HTML
+    const a = document.createElement('a')
+ 
+    // Passing the blob downloading url
+    a.setAttribute('href', url)
+ 
+    // Setting the anchor tag attribute for downloading
+    // and passing the download file name
+    a.setAttribute('download', 'tp_'+ID+'_data.csv');
+ 
+    // Performing a download with click
+    a.click()
+}
+ 
+const csvmaker = function (data) {
+ 
+    // Empty array for storing the values
+    csvRows = [];
+ 
+    // Headers is basically a keys of an
+    // object which is id, name, and
+    // profession
+    const headers = Object.keys(data);
+ 
+    // As for making csv format, headers
+    // must be separated by comma and
+    // pushing it into array
+    csvRows.push(headers.join(','));
+ 
+    // Pushing Object values into array
+    // with comma separation
+    const values = Object.values(data).join(',');
+    csvRows.push(values)
+ 
+    // Returning the array joining with new line
+    return csvRows.join('\n')
+}
